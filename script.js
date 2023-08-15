d5731211e9f5463e68dbf232334440d345715a92
xjqhdjq <xjqhdjq@gmail.com>
2023-08-15 14:05:30 +0900

    index, script

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello JavaScript");
+
+var age = 30;
+let year = 2023;
+const point = 250;
+
+console.age("Hello JavaScript");
+console.year("Hello JavaScript");
+console.point("Hello JavaScript");
+
+const pass = true;
+
+if (pass){
+    console.log("이프문 true");
+}
